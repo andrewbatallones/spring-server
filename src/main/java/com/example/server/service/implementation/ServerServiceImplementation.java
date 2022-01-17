69c50cfca2bebcca07ef1dc7cdddcf88cdbdccb6
Andrew Batallones <batallonesa@gmail.com>
2022-01-17 08:18:49 -0800

    Only use single server image

--- src/main/java/com/example/server/service/implementation/ServerServiceImplementation.java
+++ src/main/java/com/example/server/service/implementation/ServerServiceImplementation.java
@@ -73,7 +73,8 @@ public class ServerServiceImplementation implements ServerService {
     }
 
     private String setServerImageUrl() {
-        String[] imageNames = {"server1.png", "server2.png", "server3.png", "server4.png"};
-        return ServletUriComponentsBuilder.fromCurrentContextPath().path("/server/image" + imageNames[new Random().nextInt(4)]).toUriString();
+//        String[] imageNames = {"server1.png", "server2.png", "server3.png", "server4.png"};
+//        return ServletUriComponentsBuilder.fromCurrentContextPath().path("/server/image" + imageNames[new Random().nextInt(4)]).toUriString();
+        return ServletUriComponentsBuilder.fromCurrentContextPath().path("/server/image/server1.png").toUriString();
     }
 }
